ad0f8939e9ed21eb6ae49a1c872f31fe82d60539
yt2014 <taoy_2007@126.com>
2015-05-07 22:52:06 +0800

    caseshow debug

--- js/caseshow.js
+++ js/caseshow.js
@@ -11,6 +11,10 @@ var pictureArrayIndex = 0;
 var type_space = new Array("pictures/keting","pictures/chufang","pictures/woshi","pictures/yangtai");
 var text_display = new Array("¿ÍÌü","³ø·¿","ÎÔÊÒ","ÑôÌ¨");
 
+
+var pictures_borders = new Array();
+var vertical_border = 0;
+
 function initPicturesArray()
 {
     for(var i=0;i<8;i++)
@@ -23,6 +27,24 @@ function initPicturesArray()
     }
 }
 
+function initBorders()
+{
+   var left_side_pictures = $("#body_rightpart").offset().left;
+   var top_side_picture = $("#body_rightpart").offset().top;
+
+   for(var i=0;i<5;i++)
+   {
+	   if(i==0)
+	   {
+               pictures_borders[i] = left_side_pictures;
+	   }
+	   else
+	   {
+               pictures_borders[i] = pictures_borders[i-1] + 150;
+	   }
+   }
+   vertical_border = top_side_picture + 120;
+}
 /*function init_display()
      {
 	 document.getElementById("displayPictures").style.display = "none";
@@ -111,6 +133,31 @@ function find_id_index(eleid)
 	return -1;
 }
 
+function find_index(positionX,PositionY)
+{
+	var ret = -1;
+   for(var i=0;i<4;i++)
+   {
+      if((pictures_borders[i]<=positionX)&&(pictures_borders[i+1]>positionX))
+      {
+	      ret = i;
+	      break;
+      }
+   }
+   if(i==4)
+   {
+	   ret = -1;
+   }
+   else
+   {
+       if(PositionY>vertical_border)
+       {
+          ret = ret+4;
+       }
+   }
+   return ret;
+}
+
 /*
 function showPopUp(event)
 {
@@ -145,8 +192,9 @@ function(){
 	$("#displayPictures").css("display","none");
 	$(".img_lable a").css("display","none");
         initPicturesArray();
-
+        initBorders();
 	//alert(picturesArray[7][0]+picturesArray[7][1]+picturesArray[7][2]+picturesArray[7][3]);
+	//alert(pictures_borders[0] + " "+ pictures_borders[1]+" "+ pictures_borders[2]+ " "+pictures_borders[3]);
         $(".img_lable").mouseover(
         function (event) {
 		     // alert("mouseenter " + event.target.children[0]);
@@ -166,21 +214,38 @@ function(){
         function (event) {
 		      //alert("click " + event.target);
 		      event.preventDefault();
-		      $("#displayPictures").css("display","");
+		      event.stopPropagation();
+		      
 	              pictureDisplayIndex = 0;
 	              pictureArrayIndex =  find_id_index(event.target.id);
-	              //alert("array index " + pictureArrayIndex);
-                      $("#displayPictures").css("background","url("+picturesArray[pictureArrayIndex][0]+")");
-		     // alert(pictureArrayIndex);
-		     $("#displayPictures p").text(text_display[0]);
-
+                   //   alert("event_target: " + event.target.id + "index found " + pictureArrayIndex);
+		      if(pictureArrayIndex!=-1)
+	              {
+		         $("#displayPictures").css("display","");
+	                 //alert("array index " + pictureArrayIndex);
+                         $("#displayPictures").css("background","url("+picturesArray[pictureArrayIndex][0]+")");
+		         // alert(pictureArrayIndex);
+		         $("#displayPictures p").text(text_display[0]);
+	             }
+	             else
+		     {
+		         pictureArrayIndex = find_index(event.pageX,event.pageY);	     			 
+                       //  alert("index found: "+pictureArrayIndex);
+			// alert("eventpoint:("+event.pageX+","+event.pageY+") X3"+ pictures_borders[3]);
+                         $("#displayPictures").css("display","");
+	                 //alert("array index " + pictureArrayIndex);
+                         $("#displayPictures").css("background","url("+picturesArray[pictureArrayIndex][0]+")");
+		         // alert(pictureArrayIndex);
+		         $("#displayPictures p").text(text_display[0]);
+		     }
 	            }
 		             );
 
 	$("#exit_img").click(
         function (event) {
-		      //alert("click " + event.target);
+		     // alert(" func exit img click " + event.target);
 		      event.preventDefault();
+		      event.stopPropagation();
 		      $("#displayPictures").css("display","none");
 		      pictureArrayIndex=0;
 		      $("#displayPictures p").text(text_display[0]);
@@ -213,7 +278,7 @@ function(){
 
 		     $("#displayPictures p").text(text_display[pictureDisplayIndex]);
 		      //alert("clicked pageX" + event.pageX+" pagemiddle"+pagemiddle);
-                     
+                    // alert(" func display img click " + event.target);
 		    //  event.preventDefault();
 		    //  $("#displayPictures").css("display","none");
 		      
