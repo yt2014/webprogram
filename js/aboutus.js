a23448925c83f6be98dc14e43fdea63721f16c97
yt2014 <taoy_2007@126.com>
2015-07-10 08:25:35 +0800

    aboutus adjust

---
+++ js/aboutus.js
@@ -0,0 +1,9 @@
+$(document).ready(function(){
+	
+	//alert("ready height is "+$(document).innerHeight()*0.05);
+	$("#div_top").height($(document).innerHeight()*0.1);
+	$("#div_body").height($(document).innerHeight()*0.8);
+	$("#div_buttom").height($(document).innerHeight()*0.1);
+        /*in aboutus page, link clicked events in the left*/
+       
+});
