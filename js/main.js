330ddc619d9dae6f9b739f2e369d345964eceda1
yt2014 <taoy_2007@126.com>
2015-07-13 17:32:59 +0800

    adjust the main interface

--- js/main.js
+++ js/main.js
@@ -2,10 +2,78 @@
 
 $(document).ready(function(){
 	
-	//alert("ready height is "+$(document).innerHeight()*0.05);
+//	alert("ready height is "+$(document).innerHeight()*0.05);
 	$("#div_top").height($(document).innerHeight()*0.08);
 	$("#div_body").height($(document).innerHeight()*0.84);
 	$("#div_buttom").height($(document).innerHeight()*0.08);
+
+       var Y_Start = $("#aboutus").offset().top; 
+       var X_Start = $("#aboutus").offset().left;
+
+       var Y_End =  $("#caseshow").offset().top;
+       var X_End =  $("#caseshow").offset().left;
+
+       var Y_div_line = $("#svg_line1").offset().top;
+       var X_div_line = $("#svg_line1").offset().left;
+
+       var X_toMove = X_Start + 50 - X_div_line;
+       var Y_toMove = Y_End + 120 - Y_div_line; 
+
+      // alert("line1 position: "+X_div_line1+" " + Y_div_line1);
+       $("#svg_line1").css({"top":Y_toMove,"left":X_toMove});       
+       $("#svg_line1").height(Y_Start-120-Y_End);
+       $("#svg_line1").width(X_End-X_Start-20);
+
+       Y_Start = $("#designer").offset().top;//exchange start and end.
+       X_Start = $("#designer").offset().left;
+
+       Y_div_line = $("#svg_line2").offset().top;
+       X_div_line = $("#svg_line2").offset().left;
+
+       X_toMove =  X_End - X_div_line + 90;
+       Y_toMove =  Y_End - Y_div_line + 60;
+
+       $("#svg_line2").css({"top":Y_toMove,"left":X_toMove});
+       $("#svg_line2").width(X_Start-X_End-70);
+
+       Y_End = $("#service").offset().top;//exchange start and end.
+       X_End = $("#service").offset().left;
+
+       Y_div_line = $("#svg_line3").offset().top;
+       X_div_line = $("#svg_line3").offset().left;
+
+       X_toMove =  X_End - X_div_line + 50;
+       Y_toMove =  Y_Start - Y_div_line + 100;
+
+       $("#svg_line3").css({"top":Y_toMove,"left":X_toMove});
+       $("#svg_line3").width(X_Start-X_End);
+
+       Y_Start = $("#hr").offset().top;//exchange start and end.
+       X_Start = $("#hr").offset().left;
+
+       Y_div_line = $("#svg_line4").offset().top;
+       X_div_line = $("#svg_line4").offset().left;
+
+       X_toMove =  X_End - X_div_line + 70;
+       Y_toMove =  Y_Start - Y_div_line + 90;
+
+       $("#svg_line4").css({"top":Y_toMove,"left":X_toMove});
+       $("#svg_line4").width(X_Start-X_End-50);
+
+       Y_Start = $("#svg_polygon").offset().top;//adjust the pic in the center
+       X_Start = $("#svg_polygon").offset().left;
+       
+       Y_End = $("#caseshow").offset().top;
+       X_End = $("#caseshow").offset().left;
+
+       var Y_End1 = $("#designer").offset().top;
+       var X_End1 = $("#designer").offset().left;
+  
+       X_toMove = (X_End + X_End1)/2 - X_Start - 135;
+       Y_toMove = (Y_End + Y_End1)/2 - Y_Start + 110;
+
+      $("#svg_polygon").css({"top":Y_toMove,"left":X_toMove});
+
         /*in aboutus page, link clicked events in the left*/
         $("#Company_Background").click(
 		function(e)
