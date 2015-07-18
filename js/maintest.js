ea958e7bec80bc7da00c31e6a41215ef05fe0059
yt2014 <taoy_2007@126.com>
2015-07-18 10:41:32 +0800

    aboutus area in main page

--- js/maintest.js
+++ js/maintest.js
@@ -1,10 +1,52 @@
+
+var countFlash = 0;
+
+var flagFlash = 0;
+
+var arColors = new Array("red","blue","blue","yellow","gray","white");
+
+function firstFlash(){
+
+ //   document.getElementById("nowDiv").innerHTML = now; //div的html是now这个字符串 
+    if(flagFlash==1)
+    {
+	//alert("flash");
+        $("#flash_area").css("background-color",arColors[countFlash]);
+        countFlash = countFlash + 1;
+        if(countFlash==6)
+	{
+            countFlash = 0;
+	}	
+    }
+    setTimeout("firstFlash()",2000); //设置过1000毫秒就是1秒，调用show方法
+}
+
+
 $(document).ready(function(){
 	
 	//alert("ready height is "+$(document).innerHeight()*0.05);
 	$("#div_top").height($(document).innerHeight()*0.1);
-	$("#div_body").height($(document).innerHeight()*0.8);
-	$("#div_buttom").height($(document).innerHeight()*0.1);
+//	$("#div_body").height($(document).innerHeight()*0.8);
+//	$("#div_buttom").height($(document).innerHeight()*0.1);
+	$("body").width($(document).innerWidth());
         /*in aboutus page, link clicked events in the left*/
+	countFlash=0;
+	flagFlash=1;
+        $("body").load(firstFlash());
+
+	$("#flash_area").mouseover(function(){
+	
+	     flagFlash = 0;
+	});
+
+	$("#flash_area").mouseout(function(){
+	
+	     flagFlash = 1;
+	});
 
        
 });
+
+
+ 
+
