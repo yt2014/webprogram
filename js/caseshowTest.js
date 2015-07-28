0bcc95fc18f0553d4b6a23ebc07f28a323076a9e
yt2014 <taoy_2007@126.com>
2015-07-28 21:19:42 +0800

    start caseshow

---
+++ js/caseshowTest.js
@@ -0,0 +1,13 @@
+$(document).ready(function(){
+	
+//	alert("ready height is "+$(document).innerHeight());
+//	$("#div_top").height(80);
+	$("body").width($(document).innerWidth());
+//        $("#div_body").height(400);
+	$("#div_body").height($(document).innerHeight()-200);
+//	$("#div_buttom").height($(document).innerHeight()*0.1);
+//alert("doc ready");
+       
+	
+      
+});
